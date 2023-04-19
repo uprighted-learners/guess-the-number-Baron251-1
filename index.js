8e41e2e7784b4f038d954abe306b8774a3048fc2
Baron <scorpiov2000@gmail.com>
2023-04-19 12:31:15 -0400

    Nothing special push, don't mind me

--- index.js
+++ index.js
@@ -10,67 +10,83 @@ function ask(questionText) {
 }
 
 // ! DO NOT TOUCH ABOVE THIS LINE
+// Don't flood the thing with async functions
 start();
-
+compGuess();
+// ! To use await ask, you must declare a variable, then "await ask (whatever)" after the `=`. The users input will change the variable from `whatever` to the users input
 async function start() {
   console.log(
     "Let's play a game where you (human) make up a number and I (computer) try to guess it."
   );
-
-  // let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-
-  // console.log('You entered: ' + secretNumber);
-
-  // Now try and complete the program.
-
-  // example async await function to ask for highest number
-  async function pickHighNum() {
-    // Set lowest num
-    const minNum = 1;
-
-    let pickMaxNum = await ask(
-      `\nPlease choose a number greater than ${minNum}: `
+}
+// ? Functionality for computer guessing
+//
+async function compGuess() {
+  // ? Define the variables
+  let minNum = 1;
+  console.log(minNum);
+  let maxNum = 100;
+  let secretNum = await ask(`Pick a number between ${minNum} and ${maxNum} _>`);
+  let numGuess = Math.round((minNum + maxNum) / 2);
+  if (secretNum < minNum || secretNum > maxNum) {
+    console.log(
+      "Please choose a number within the specified range, or learn how to count."
     );
-
-    // Grab the value of user input
-    let highNum = parseInt(pickMaxNum);
-
-    // Confirmation message to print
-    console.log(`\nYou set ${highNum} as the highest value.`);
-
-    async function secretInput() {
-      let secretNumber = await ask(`What will your secret number be?`);
-      numGuess(); // at the end of secretInput running, start running numGuess
-    }
-    function numGen() {
-      return Math.floor(Math.random() * highNum + 1);
-    }
-
-    async function numGuess() {
-      while (numGen !== numGuess){
-      let result = numGen();
-      let guessedNum = await ask(
-        `Now, allow me to guess a number between ${minNum}, and ${highNum}...Is your number ${result}?`
-      );
-      // call y or n function, if no, run h or lower
-
-      // we need a yes or no answer from user if result is their number, create await async to ask y or n
-      let highLow = "Did I get it?"   
-      let answer = await ask(highLow)
-         if (answer === "y" || answer === "Y") {
-          console.log("I did it!")
-          process.exit();
-         } else {
-          console.log("Is your number higher, or lower?")
-         }
-         
-      // if no, we need to use higher or lower condtional (we can reassign highest/lowest value in conditionl) create await async
+  } else {
+    let input = await ask(
+      `Let us begin. Is your number ${numGuess}? "y", or "n" _>`
+    );
+    // ? Logic for computer win
+    if (input == "y" && numGuess == secretNum) {
+      console.log("I did it!");
+      process.exit();
     }
   }
+
+  // ? Logic for computer wrong answer/modify range
+  while (numGuess !== secretNum) {
+    let answer = await ask("Is your number higher(h) or lower(l)? _>");
+    console.log(`numGuess: ${numGuess}`)
+    if (answer === "h" && secretNum > numGuess) {
+      minNum = numGuess + 1;
+      numGuess = Math.round((minNum + maxNum) / 2);
+      let input = await ask(
+        `Alright. Is your number ${numGuess}? "y", or "n" _>`
+      );
+      if (input == "n" && numGuess == secretNum){
+        console.log(`I smell smoke. Are your pants on fire? ... LIAR, your number IS ${numGuess}, I WIN`)
+        process.exit()
+      }
+      // console.log(`Reminder, your number is ${secretNum}`)
+      // ? Logic for computer win
+      else if (input == "y" && numGuess == secretNum) {
+        console.log("I did it!");
+        process.exit();
+      }
+    } else if (answer === "l" && secretNum < numGuess) {
+      maxNum = numGuess - 1;
+      numGuess = Math.round((minNum + maxNum) / 2);
+      let input = await ask(
+        `Alright. Is your number ${numGuess}? "y", or "n" _>`
+        );
+        // console.log(`Reminder, your number is ${secretNum}`)
+      // ? Logic for computer win
+      if (input == "n" && numGuess == secretNum){
+        console.log(`I smell smoke. Are your pants on fire? ... LIAR, your number IS ${numGuess}, I WIN`)
+        process.exit()
+      }
+      else if (input == "y" && numGuess == secretNum) {
+        console.log("I did it!");
+        process.exit();
+      }
+    } else {
+      console.log(
+        "You had two options, and you just had to be a rebel. 'h', or 'l'. _>"
+      );
+      // console.log(`Your number is ${secretNum}`)
+    }
+    // console.log(minNum);
+  }
+
   
-  secretInput();
-  
-  
-} // While loop
-pickHighNum();
-} // Async function
+} // ! compGuess end
